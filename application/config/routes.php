f18fca35dfa0d4d63cd3ed88d0661dbd895b5eb3
ktsuench <will289@hotmail.com>
2017-01-21 09:46:40 -0500

    updated codeigniter to rest api version

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['api/items/(.+)']['get'] = 'api/items/$1';
-$route['api/items/(.+)']['post'] = 'api/items/$1/$2';
+
+/*
+| -------------------------------------------------------------------------
+| Sample REST API Routes
+| -------------------------------------------------------------------------
+*/
+$route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
+$route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
