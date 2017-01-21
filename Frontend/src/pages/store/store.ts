aff787ee8bf4735416e4926f9bb4f96d40e1e3e0
jas2996 <JaskiranLamba@Jaskirans-MacBook-Pro.local>
2017-01-21 03:00:56 -0500

    updated front end

---
+++ Frontend/src/pages/store/store.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-store',
+  templateUrl: 'store.html'
+})
+export class StorePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
