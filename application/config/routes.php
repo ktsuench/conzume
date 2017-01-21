c77d779e8cce7eedecf2f5abae84127f2f81dc90
ktsuench <will289@hotmail.com>
2017-01-21 10:19:24 +0000

    updated db, routes config and added in item controller/model

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['api/items/(.+)']['get'] = 'api/items/$1';
+$route['api/items/(.+)']['post'] = 'api/items/$1/$2'
